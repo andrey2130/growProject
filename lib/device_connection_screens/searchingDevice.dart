0333b9c40d637923e9d9ee043387f8cf37aa0b08
andrey2130 <andriy.2130duda@gmail.com>
2024-12-03 01:02:36 +0200

    update connection screen and add searching devices screen

---
+++ lib/device_connection_screens/searchingDevice.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SearchingDevice extends StatefulWidget {
+  const SearchingDevice({super.key});
+
+  @override
+  State<SearchingDevice> createState() => _SearchingDeviceState();
+}
+
+class _SearchingDeviceState extends State<SearchingDevice> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Text('sadasd')),
+    );
+  }
+}
